9eeba60c810eb47907cea2adff996584abba769e
Mohammad Taradeh <98043145+Mohammad-taradeh@users.noreply.github.com>
2024-09-26 10:32:36 +0300

    create the ticket repository

---
+++ Model/Repositories/TicketRepository.cs
@@ -0,0 +1,25 @@
+﻿using AirportTicketBooking.Model.Classes;
+
+namespace AirportTicketBooking.Model.Repositories;
+
+public static class TicketRepository
+{
+    private static List<Ticket> _tickets = [];
+
+    public static void ReadTicketsFromFile()
+    {
+        _tickets = new List<Ticket>()
+        {
+            new(){ Flight = new Flight { Class = new FlightClass(){Type = Utils.FlightClassType.Business, Price = 100,  Seats = 100},
+        DepartureAirport = Utils.Airport.QueenAliaInternationalAirport,
+        DestinationAirport = Utils.Airport.QueenAliaInternationalAirport,
+        Time = DateTime.Now.AddMinutes(50)},
+        DepartureAirport = Utils.Airport.QueenAliaInternationalAirport,
+        DestinationAirport = Utils.Airport.QueenAliaInternationalAirport, Time = DateTime.Now .AddMinutes(50) } };
+    }
+    public static List<Ticket> GetAllTickets ()
+    {
+        ReadTicketsFromFile();
+        return _tickets;
+    }
+}
