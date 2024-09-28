010fb5d4f4a3a5989b7ec17eab61a0a7d052cc48
Mohammad Taradeh <98043145+Mohammad-taradeh@users.noreply.github.com>
2024-09-28 17:50:48 +0300

    create default view to login and map to the needed view-model

---
+++ View/DefaultView.cs
@@ -0,0 +1,33 @@
+﻿using AirportTicketBooking.Model.Classes;
+using AirportTicketBooking.Model.Repositories;
+using AirportTicketBooking.View;
+
+namespace AirportTicketBooking.ViewModel;
+
+public static class DefaultViewModel
+{
+    private static List<User> users = UserRepository.GetAllUsers();
+    public static void Login(String email, String password)
+    {
+        try
+        {
+            var authenticatedUser =  users.FirstOrDefault(user => user.Email == email && user.Password == password);
+            if (authenticatedUser == null) {
+                Console.WriteLine("Failed to login.");
+            }
+            else if(authenticatedUser.Role == Utils.UserRole.ADMIN)
+            {
+                //admin view display feature.
+            }
+            else
+            {
+                PassengerView._user = authenticatedUser;
+                PassengerViewModel.passenger = authenticatedUser;
+                PassengerView.DisplayFeatures();
+            }
+        }
+        catch (Exception ex) {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
