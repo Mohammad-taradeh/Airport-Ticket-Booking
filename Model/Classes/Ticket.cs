f856e627e159275645c9455b986eae087c32bea2
Mohammad Taradeh <98043145+Mohammad-taradeh@users.noreply.github.com>
2024-10-01 14:14:01 +0300

    update the flow of tickets

--- Model/Classes/Ticket.cs
+++ Model/Classes/Ticket.cs
@@ -7,18 +7,18 @@ public class Ticket
 {
     private static long _id = 0;
     public long Id { get; init; } = ++_id;
-    public required Flight Flight { get; set; }
+    public required long Passenger { get; init; }
+    public required long Flight { get; set; }
     public required Airport DepartureAirport { get; set; }
     public required Airport DestinationAirport { get; set; }
     public required DateTime Time { get; set; }
     public override string ToString()
     {
         return $"Ticket ID: {Id}\n" +
-            $"Flight ID: {Flight?.Id}\n" +
+            $"Flight ID: {Flight}\n" +
             $"Departure airport: {DepartureAirport}" +
             $"Destination airport: {DestinationAirport}" +
-            $"At: {Time}\n" +
-            $"{Flight?.Class.Type} Class";
+            $"At: {Time}\n";
     }
 
 }
---
+++ Model/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace AirportTicketBooking.Model.Repositories;
+
+public interface IRepository<T> where T : class
+{
+    public abstract T? Save(T item);
+    public abstract T? Delete(T item);
+    public abstract T? Get(long id);
+    public abstract T? Update(long oldID, T newItem);
+    public abstract List<T>? GetAll();
+
+}
