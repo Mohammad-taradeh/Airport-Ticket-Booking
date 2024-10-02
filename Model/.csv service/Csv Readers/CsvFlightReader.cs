9a72ed044a5d3a73c31df2064baa7ea338fcdbd6
Mohammad Taradeh <98043145+Mohammad-taradeh@users.noreply.github.com>
2024-10-02 23:36:59 +0300

    re-arrange the code

--- Model/.csv service/Csv Readers/CsvFlightReader.cs
+++ Model/.csv service/Csv Readers/CsvFlightReader.cs
@@ -9,27 +9,19 @@ public class CsvFlightReader : ICsvReader<Flight>
 {
     public List<Flight> Read()
     {
-        using (var streamReader = new StreamReader(@"C:\Users\Lenovo\source\repos\AirportTicketBookingSolution\data\flights.csv>"))
-        {
-            using (var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture))
-            {
-                csvReader.Context.RegisterClassMap<FlightClassMap>();
-                return csvReader.GetRecords<Flight>().ToList();
-            }
-        }
+        using var streamReader = new StreamReader(@"C:\Users\Lenovo\source\repos\AirportTicketBookingSolution\data\flights.csv>");
+        using var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture);
+        csvReader.Context.RegisterClassMap<FlightClassMap>();
+        return csvReader.GetRecords<Flight>().ToList();
     }
 
     public bool Write(List<Flight> data)
     {
         var flightsCsvPath = Path.Combine(Environment.CurrentDirectory, $"flights.csv");
-        using (var streamWriter = new StreamWriter(flightsCsvPath))
-        {
-            using (var csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture))
-            {
-                csvWriter.Context.RegisterClassMap<FlightClassMap>();
-                csvWriter.WriteRecord(data);
-                return true;
-            }
-        }
+        using var streamWriter = new StreamWriter(flightsCsvPath);
+        using var csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture);
+        csvWriter.Context.RegisterClassMap<FlightClassMap>();
+        csvWriter.WriteRecord(data);
+        return true;
     }
 }
--- Model/.csv service/Csv Readers/CsvTicketReader.cs
+++ Model/.csv service/Csv Readers/CsvTicketReader.cs
@@ -9,27 +9,19 @@ public class CsvTicketReader: ICsvReader<Ticket>
 {
     public List<Ticket> Read()
     {
-        using (var streamReader = new StreamReader(@"C:\Users\Lenovo\source\repos\AirportTicketBookingSolution\data\tickets.csv>"))
-        {
-            using (var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture))
-            {
-                csvReader.Context.RegisterClassMap<TicketClassMap>();
-                return csvReader.GetRecords<Ticket>().ToList();
-            }
-        }
+        using var streamReader = new StreamReader(@"C:\Users\Lenovo\source\repos\AirportTicketBookingSolution\data\tickets.csv>");
+        using var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture);
+        csvReader.Context.RegisterClassMap<TicketClassMap>();
+        return csvReader.GetRecords<Ticket>().ToList();
     }
 
     public bool Write(List<Ticket> data)
     {
         var ticketsCsvPath = Path.Combine(Environment.CurrentDirectory, $"tickets.csv");
-        using (var streamWriter = new StreamWriter(ticketsCsvPath))
-        {
-            using (var csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture))
-            {
-                csvWriter.Context.RegisterClassMap<TicketClassMap>();
-                csvWriter.WriteRecord(data);
-                return true;
-            }
-        }
+        using var streamWriter = new StreamWriter(ticketsCsvPath);
+        using var csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture);
+        csvWriter.Context.RegisterClassMap<TicketClassMap>();
+        csvWriter.WriteRecord(data);
+        return true;
     }
 }
